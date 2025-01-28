a20b9448cf7bcc04ad182dc86d2c4676d7b7d583
Ziyu Guo <guoziyu0716@hotmail.com>
2025-01-28 09:49:21 -0800

    simple client server

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+client= socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(("<MAC_ADDR", 4))
+
+try:
+    while True:
+        message = input("Enter message from Client side: ")
+        client.send(message.encode("utf-8"))
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message received on CLIENT SIDE: {data.decode("utf-8")}")
+except OSError as e:
+    pass
+
+client.close()
---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+server.bind(("<MAC_ADDR", 4))
+server.listen(1)
+
+client, addr = server.accept()
+
+try:
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        print(f"Message Received on SERVER side: {data.decode('utf-8')}")
+        message = input ("ENTER MESSAGE ON SERVER SIDE:")
+        client.send(message.encode("utf-8"))
+except OSError as e:
+    pass
+
+client.close()
+server.close()
